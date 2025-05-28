391bbcb0c7d3630df1d3826e1f72608390a2d157
Nathan Samuell <nathansamuell@duck.com>
2025-05-28 12:13:05 -0500

    initial commit

---
+++ hw1/tdrss_names_v01.tf
@@ -0,0 +1,170 @@
+KPL/FK
+
+   Version 1.0, 2014-OCT-10, BVS/NAIF
+
+   This text kernel defines the following NAIF name/ID mappings for 
+   the TDRSS spacecraft:
+
+       Name   NAIF ID   Synonyms
+      ------  -------  ------------------------
+       TDR1      -4    TDRS-1,  TDRS-01, TDRS-A 
+       TDR3    -124    TDRS-3,  TDRS-03, TDRS-C 
+       TDR4    -128    TDRS-4,  TDRS-04, TDRS-D 
+       TDR5    -132    TDRS-5,  TDRS-05, TDRS-E 
+       TDR6    -136    TDRS-6,  TDRS-06, TDRS-F 
+       TDR7    -137    TDRS-7,  TDRS-07, TDRS-G 
+       TDR8    -138    TDRS-8,  TDRS-08, TDRS-H 
+       TDR9    -139    TDRS-9,  TDRS-09, TDRS-I
+       TD10    -141    TDRS-10,          TDRS-J 
+       TD11    -220    TDRS-11,          TDRS-K 
+       TD12    -221    TDRS-12,          TDRS-L 
+       TD13    -222    TDRS-13,          TDRS-M 
+       TD14    -223    TDRS-14,          TDRS-N 
+
+   \begindata
+
+      NAIF_BODY_NAME += ( 'TDRS-A'  )
+      NAIF_BODY_CODE += ( -4        )
+ 
+      NAIF_BODY_NAME += ( 'TDRS-01' )
+      NAIF_BODY_CODE += ( -4        )
+ 
+      NAIF_BODY_NAME += ( 'TDRS-1'  )
+      NAIF_BODY_CODE += ( -4        )
+ 
+      NAIF_BODY_NAME += ( 'TDR1'    )
+      NAIF_BODY_CODE += ( -4        )
+
+      NAIF_BODY_NAME += ( 'TDRS-C'  )
+      NAIF_BODY_CODE += ( -124      )
+ 
+      NAIF_BODY_NAME += ( 'TDRS-03' )
+      NAIF_BODY_CODE += ( -124      )
+ 
+      NAIF_BODY_NAME += ( 'TDRS-3'  )
+      NAIF_BODY_CODE += ( -124      )
+ 
+      NAIF_BODY_NAME += ( 'TDR3'    )
+      NAIF_BODY_CODE += ( -124      )
+
+      NAIF_BODY_NAME += ( 'TDRS-D'  )
+      NAIF_BODY_CODE += ( -128      )
+
+      NAIF_BODY_NAME += ( 'TDRS-04' )
+      NAIF_BODY_CODE += ( -128      )
+
+      NAIF_BODY_NAME += ( 'TDRS-4'  )
+      NAIF_BODY_CODE += ( -128      )
+
+      NAIF_BODY_NAME += ( 'TDR4'    )
+      NAIF_BODY_CODE += ( -128      )
+
+      NAIF_BODY_NAME += ( 'TDRS-E'  )
+      NAIF_BODY_CODE += ( -132      )
+
+      NAIF_BODY_NAME += ( 'TDRS-05' )
+      NAIF_BODY_CODE += ( -132      )
+
+      NAIF_BODY_NAME += ( 'TDRS-5'  )
+      NAIF_BODY_CODE += ( -132      )
+
+      NAIF_BODY_NAME += ( 'TDR5'    )
+      NAIF_BODY_CODE += ( -132      )
+
+      NAIF_BODY_NAME += ( 'TDRS-F'  )
+      NAIF_BODY_CODE += ( -136      )
+
+      NAIF_BODY_NAME += ( 'TDRS-06' )
+      NAIF_BODY_CODE += ( -136      )
+
+      NAIF_BODY_NAME += ( 'TDRS-6'  )
+      NAIF_BODY_CODE += ( -136      )
+
+      NAIF_BODY_NAME += ( 'TDR6'    )
+      NAIF_BODY_CODE += ( -136      )
+
+      NAIF_BODY_NAME += ( 'TDRS-G'  )
+      NAIF_BODY_CODE += ( -137      )
+
+      NAIF_BODY_NAME += ( 'TDRS-07' )
+      NAIF_BODY_CODE += ( -137      )
+
+      NAIF_BODY_NAME += ( 'TDRS-7'  )
+      NAIF_BODY_CODE += ( -137      )
+
+      NAIF_BODY_NAME += ( 'TDR7'    )
+      NAIF_BODY_CODE += ( -137      )
+
+      NAIF_BODY_NAME += ( 'TDRS-H'  )
+      NAIF_BODY_CODE += ( -138      )
+
+      NAIF_BODY_NAME += ( 'TDRS-08' )
+      NAIF_BODY_CODE += ( -138      )
+
+      NAIF_BODY_NAME += ( 'TDRS-8'  )
+      NAIF_BODY_CODE += ( -138      )
+
+      NAIF_BODY_NAME += ( 'TDR8'    )
+      NAIF_BODY_CODE += ( -138      )
+
+      NAIF_BODY_NAME += ( 'TDRS-I'  )
+      NAIF_BODY_CODE += ( -139      )
+
+      NAIF_BODY_NAME += ( 'TDRS-09' )
+      NAIF_BODY_CODE += ( -139      )
+
+      NAIF_BODY_NAME += ( 'TDRS-9'  )
+      NAIF_BODY_CODE += ( -139      )
+
+      NAIF_BODY_NAME += ( 'TDR9'    )
+      NAIF_BODY_CODE += ( -139      )
+
+      NAIF_BODY_NAME += ( 'TDRS-J'  )
+      NAIF_BODY_CODE += ( -141      )
+
+      NAIF_BODY_NAME += ( 'TDRS-10' )
+      NAIF_BODY_CODE += ( -141      )
+
+      NAIF_BODY_NAME += ( 'TD10'    )
+      NAIF_BODY_CODE += ( -141      )
+
+      NAIF_BODY_NAME += ( 'TDRS-K'  )
+      NAIF_BODY_CODE += ( -220      )
+
+      NAIF_BODY_NAME += ( 'TDRS-11' )
+      NAIF_BODY_CODE += ( -220      )
+
+      NAIF_BODY_NAME += ( 'TD11'    )
+      NAIF_BODY_CODE += ( -220      )
+
+      NAIF_BODY_NAME += ( 'TDRS-L'  )
+      NAIF_BODY_CODE += ( -221      )
+
+      NAIF_BODY_NAME += ( 'TDRS-12' )
+      NAIF_BODY_CODE += ( -221      )
+
+      NAIF_BODY_NAME += ( 'TD12'    )
+      NAIF_BODY_CODE += ( -221      )
+
+      NAIF_BODY_NAME += ( 'TDRS-M'  )
+      NAIF_BODY_CODE += ( -222      )
+
+      NAIF_BODY_NAME += ( 'TDRS-13' )
+      NAIF_BODY_CODE += ( -222      )
+
+      NAIF_BODY_NAME += ( 'TD13'    )
+      NAIF_BODY_CODE += ( -222      )
+
+      NAIF_BODY_NAME += ( 'TDRS-N'  )
+      NAIF_BODY_CODE += ( -223      )
+
+      NAIF_BODY_NAME += ( 'TDRS-14' )
+      NAIF_BODY_CODE += ( -223      )
+
+      NAIF_BODY_NAME += ( 'TD14'    )
+      NAIF_BODY_CODE += ( -223      )
+
+   \begintext
+
+End of text kernel.
+ 
