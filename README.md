3f663e2d9d4f37e4c926cfed243bdc7ad0184cce
Nathan Samuell <nathansamuell@duck.com>
2025-05-29 12:16:12 -0500

    mostly finished with problem 2\!

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
     - [SPICE CONCEPTS](https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/Tutorials/pdf/individual_docs/04_concepts.pdf): good conceptual intro material
 - [MrDrProfPaul (Paul McKee)](https://www.youtube.com/@MrDrProfPaul)
 
+### HW1 Answers!
+https://github.com/nathansamuell/OpNav/blob/main/hw1/output.txt
+
--- hw1/script.c
+++ hw1/script.c
@@ -9,13 +9,13 @@ int main() {
 
 	/* local variables */
 	SpiceChar leapSeconds_KernelFile [FILE_SIZE] = "naif0012.tls";	// file name
-	SpiceChar tdrssSPK_KernelFile [FILE_SIZE] = "TDRSS.bsp";		// file name
-	SpiceChar observer[WORD_SIZE] = "EARTH";						// name of observer
-	SpiceChar target[WORD_SIZE] = "-141";							// TDRSS-10 (ID -141 via the names file)
-	SpiceChar time_UTC[WORD_SIZE] = "2023 JAN 01 12:00:00";			// Time we want position at
-	SpiceDouble lightTime;											// not strictly needed for us, but needed for the function
-	SpiceDouble time_ET;											// variable to store ephemeris time
-	SpiceDouble stateData[6];										// used to store state data
+	SpiceChar tdrssSPK_KernelFile [FILE_SIZE] = "TDRSS.bsp";	// file name
+	SpiceChar observer[WORD_SIZE] = "EARTH";			// name of observer
+	SpiceChar target[WORD_SIZE] = "-141";				// TDRSS-10 (ID -141 via the names file)
+	SpiceChar time_UTC[WORD_SIZE] = "2023 JAN 01 12:00:00";		// Time we want position at
+	SpiceDouble lightTime;						// not strictly needed for us, but needed for the function
+	SpiceDouble time_ET;						// variable to store ephemeris time
+	SpiceDouble stateData[6];					// used to store state data
 
 	/* load kernels */
 	furnsh_c(leapSeconds_KernelFile);
@@ -45,7 +45,8 @@ int main() {
 	// resuse ephemeris time from problem 1
 	SpiceDouble mu; // found in Gravity.tpc
 	SpiceInt elementsFound; // not needed, just here to satisfy bodvrd_c
-	SpiceDouble orbitalElements[8];
+	SpiceDouble orbitalConstants[8];
+	SpiceDouble a;
 	SpiceChar gravityKernel_KernelFile[FILE_SIZE] = "Gravity.tpc";
 	
 	/* load gravity kernel! */
@@ -54,8 +55,41 @@ int main() {
 	/* retrieve gravity constant mu from Gravity Kernel*/
 	bodvrd_c("EARTH", "GM", 1, &elementsFound, &mu);
 
+	/* ORBITAL ELEMENTS NEEDED 
+	 * - theta (angle of orbit)
+	 * - e (eccentricity)
+	 * - a (semi-major axis)
+	 * - Big Omega (orientation)
+	 * - Little Omega (orientation)
+	 * - i (orientation)*
+	 * WHERE TO FIND
+	 * e is orbitalConstants[1] according to SPICE docs*
+	 * Big Omega, or RAAN (longitude of AN in SPICE docs*) is orbitalConstants[3]
+	 * i is inclination in SPICE docs, or orbitalConstants[2]
+	 * little omega is argument of perigee/periapsis, orbitalConstants[4] in SPICE docs
+	 * from useful equations in orbital mechanics review, a (semi-major axis) = perifocal distance / (1 - e),
+	 *	so computing should be straightforward! (perifocal distance is orbitalConstants[0] in SPICE docs)
+	 * theta can be found from solving kepler's equation!
+	 * */
+	
 	/* get orbital constants! */
-	oscelt_c(stateData, time_ET, mu, orbitalElements);
+	oscelt_c(stateData, time_ET, mu, orbitalConstants);
+
+	/* calculate a */
+	a = orbitalConstants[0] / (1.0 - orbitalConstants[1]);
+
+	/* solve Kepler's equation for theta */
+	// TODO: figure out if this is the correct approach
+	
+
+	/* print out our results */
+	printf("\nPROBLEM B: ORBITAL ELEMENTS\n");
+	printf("Theta: %23.16e\n", 23.894); // temp variable until I can calculate it
+	printf("a: %23.16e\n", a);
+	printf("e: %23.16e\n", orbitalConstants[1]);
+	printf("Big Omega: %23.16e\n", orbitalConstants[3]);
+	printf("i: %23.16e\n", orbitalConstants[2]);
+	printf("Little Omega: %23.16e\n", orbitalConstants[4]);
 	
 
 	return 0;
